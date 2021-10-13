3e3c7ae8a2221d817a3db22eeca01074dd82a219
ymclapp <ymclapp@yahoo.com>
2021-10-13 14:00:34 -0500

    built base file

---
+++ PhilLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace PhilLibrary.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        //Proof of life
+        int zero = 1 - 2;
+        int num = 7 / zero;
+        }
+    }
+}
---
+++ PhilLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhilLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
